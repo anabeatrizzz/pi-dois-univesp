688060ba913bdbeb9a05feca1d865f9c2d48f602
Ana Beatriz <anabeatriz.augusto06@yahoo.com>
2022-05-27 19:52:10 -0300

    company page responsiveness

--- src/pages/company/Company.tsx
+++ src/pages/company/Company.tsx
@@ -1,4 +1,4 @@
-import { Grid, Typography, Card, CardContent } from '@mui/material';
+import { Grid, Typography, Card, CardContent, useMediaQuery, useTheme } from '@mui/material';
 import React from 'react';
 import WrapperPage from '../../components/wrapper-page';
 import styles from './Company.css';
@@ -10,6 +10,9 @@ import bestProvider from '../../assets/melhor-provedor.png';
 import bestSpeed from '../../assets/melhor-velocidade.png';
 
 export default function Company() {
+  const theme = useTheme();
+  const matches = useMediaQuery(theme.breakpoints.down('sm'));
+
   const settings = {
     dots: false,
     slidesToShow: 3,
@@ -17,17 +20,35 @@ export default function Company() {
     arrows: true
   }
 
+  function txtSize(){
+    if(matches){
+      return {
+        ...styles.sliderTitle,
+        fontSize: 14
+      }
+    } else {
+      return {
+        ...styles.sliderTitle
+      }
+    }
+  }
+
   return (
     <WrapperPage>
       <div style={styles.grid}>
-
         <Grid style={styles.containerWrapper} container>
           <Grid style={styles.container} container>
-            <Typography style={styles.title} align="center">
+            <Typography
+              style={matches ? { ...styles.title, fontSize: 20 } : styles.title}
+              align="center"
+            >
               SOBRE A PAMNET
             </Typography>
             <div style={styles.subtitleDiv}>
-              <Typography style={styles.subtitle} align="center">
+              <Typography
+                style={matches ? { ...styles.subtitle, fontSize: 20 } : styles.subtitle}
+                align="center"
+              >
                 Fundada em 1995 a Pam Pépe Antenas é hoje uma empresa sólida e arrojada na implantação de malha óptica na cidade de Mongaguá para provimento de acesso à Internet em banda larga. Se especializou em oferecer durante alguns anos sinal de internet via rádio cobrindo da Pedreira a Agenor de Campos e a Zona Rural. Hoje com sede própria e estruturada, a Pam Pépe Antenas possui uma equipe de profissionais treinados e capacitados para atendimento técnico e comercial.
               </Typography>
             </div>
@@ -36,42 +57,42 @@ export default function Company() {
         </Grid>
 
         <Grid container>
-          <Grid item xs={6}>
+          <Grid item xs={matches ? 11 : 6}>
             <Slider {...settings}>
               <div>
-                <Typography align="center" style={styles.sliderTitle}>
+                <Typography align="center" style={txtSize()}>
                   MISSÃO
                 </Typography>
-                <Typography style={{ ...styles.subtitle, fontSize: 16 }}>
+                <Typography style={styles.sliderSubtitle}>
                   Ser reconhecida pelos funcionários, usuários e pela comunidade como a melhor empresa provedora de serviços de internet via fibra óptica da região.
                 </Typography>
               </div>
               <div>
-                <Typography align="center" style={styles.sliderTitle}>
+                <Typography align="center" style={txtSize()}>
                   VISÃO
                 </Typography>
                 <Typography style={styles.sliderSubtitle}>Competência, qualidade, eficiência e profissionalismo nos atendimentos e nos serviços prestados aos usuários e colaboradores.</Typography>
               </div>
               <div>
-                <Typography align="center" style={styles.sliderTitle}>
+                <Typography align="center" style={txtSize()}>
                   VALORES
                 </Typography>
                 <Typography style={styles.sliderSubtitle}>✓ Conduta ética e profissional<br />✓ Transparência nas atitudes e comportamentos<br />✓ Busca constante da excelência no atendimento<br />✓ Aprimoramento técnico e profissional constante</Typography>
               </div>
               <div>
-                <Typography align="center" style={styles.sliderTitle}>
+                <Typography align="center" style={txtSize()}>
                   EMPREENDEDORISMO
                 </Typography>
                 <Typography style={styles.sliderSubtitle}>Como empreendedores em nossa cidade, Mongaguá, procuramos sempre crescer. Nossa conquista vem de uma prospecção de usuários muito boa.</Typography>
               </div>
               <div>
-                <Typography align="center" style={styles.sliderTitle}>
+                <Typography align="center" style={txtSize()}>
                   QUALIDADE DE INVESTIMENTO
                 </Typography>
                 <Typography style={styles.sliderSubtitle}>Primeiro a instalação da base e repetidoras. Saímos da LP de dados para link em fibra óptica com capacidade de 500 Mbps. Hoje em 2020 contamos com mais de 40 Gbps em fibra óptica chegando em nossa sede para ser distribuído por nossos usuários.</Typography>
               </div>
               <div>
-                <Typography align="center" style={styles.sliderTitle}>
+                <Typography align="center" style={txtSize()}>
                   ATENDIMENTO
                 </Typography>
                 <Typography style={styles.sliderSubtitle}>
@@ -82,8 +103,11 @@ export default function Company() {
 
           </Grid>
 
-          <Grid item xs={6}>
-            <Typography align="center" style={styles.cardTitle}>
+          <Grid item xs={matches ? 12 : 6}>
+            <Typography
+              align="center"
+              style={matches ? { ...styles.cardTitle, marginTop: 20 } : styles.cardTitle}
+            >
               RELATOS DOS CLIENTES
             </Typography>
             <Card style={styles.card}>
